a1141e2f291fa8728dcbec74472a7df7b6e2e43b
Mario Martinez <mario.martinez@peoplewalking.com>
2019-09-06 10:46:35 -0600

    Add simplesamlphp example settings

---
+++ simplesamlphp/saml20-idp-remote.php
@@ -0,0 +1,35 @@
+<?php
+
+// metadata/saml20-idp-remote.php
+
+$metadata['https://simplesamlphp.local/simplesaml/saml2/idp/metadata.php'] = array (
+  'metadata-set' => 'saml20-idp-remote',
+  'entityid' => 'https://simplesamlphp.local/simplesaml/saml2/idp/metadata.php',
+  'SingleSignOnService' =>
+  array (
+    0 =>
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+      'Location' => 'https://simplesamlphp.local/simplesaml/saml2/idp/SSOService.php',
+    ),
+  ),
+  'SingleLogoutService' =>
+  array (
+    0 =>
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+      'Location' => 'https://simplesamlphp.local/simplesaml/saml2/idp/SingleLogoutService.php',
+    ),
+  ),
+  'certData' => 'MIIFGTCCA4GgAwIBAgIUX5pFZ/nocXXOuXci5EbGjIgZYkEwDQYJKoZIhvcNAQELBQAwgZsxCzAJBgNVBAYTAkFVMRAwDgYDVQQIDAdNYW5hZ3VhMRAwDgYDVQQHDAdNYW5hZ3VhMQwwCgYDVQQKDANQUFcxCzAJBgNVBAsMAklUMRwwGgYDVQQDDBNzaW1wbGVzYW1scGhwLmxvY2FsMS8wLQYJKoZIhvcNAQkBFiBtYXJpby5tYXJ0aW5lekBwZW9wbGV3YWxraW5nLmNvbTAeFw0xOTA5MDQxOTIxMzBaFw0yOTA5MDMxOTIxMzBaMIGbMQswCQYDVQQGEwJBVTEQMA4GA1UECAwHTWFuYWd1YTEQMA4GA1UEBwwHTWFuYWd1YTEMMAoGA1UECgwDUFBXMQswCQYDVQQLDAJJVDEcMBoGA1UEAwwTc2ltcGxlc2FtbHBocC5sb2NhbDEvMC0GCSqGSIb3DQEJARYgbWFyaW8ubWFydGluZXpAcGVvcGxld2Fsa2luZy5jb20wggGiMA0GCSqGSIb3DQEBAQUAA4IBjwAwggGKAoIBgQDAFr01yuB1/QQwY5RAxv0brEYVoM78Oq6TcQlYM5BD8Vv3fLTjpKILciD7VvxF3QAojcb1Poa9vupJENqWXnpv7gzNzlkor3MNBVV0Pj8Bn2lvIczzJTocMgHXG460pIjI9RjVS0QZNVldvhmjZ7JHsyM9VVyYvYGh7h9RksgvqSFMHrkq1WSaeUNHmiIYkod+yutfy31LOtsz/KCOUCmpYW7jpx2YcbKnysrSPsDbfwGNFCzlJOb/Q1tF2BuzyxAeVyH4Q88Rh+KLzjS0ukTFiWj26u+WVxuFl16u4bIZbZjMlzU3FZLzuuAsBSh0hpyadmNrae3eyR+y2aD/yaOjN/SpRwpoIyfWyyclNko6QzxN3+l7dFVZo2yDSq+Z8PctP2S2R54ogWKQVhCCds59QKAEvFBhbBH9PyidI+5JzZl82XmTMn6WqHg15Earj5xXjS+XnJWQafZbQrm1RJD015yWdkjPickjyLtXSK+P+XbmIqxQt6VDQDMczx0wx8cCAwEAAaNTMFEwHQYDVR0OBBYEFNlFLL1lh5poQJs99oIUqsAjUMKjMB8GA1UdIwQYMBaAFNlFLL1lh5poQJs99oIUqsAjUMKjMA8GA1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQELBQADggGBAADOn073Qlct4mkGEH6on+2EZoyhf6RM1Tk3lDzXrbcOTxPKeUiSnXFXDObhDOzwEb2O7mMrVg9EHlMHDf4mhdwAqoMM06NB/D10Vghv20QShV/ZR8S6ePYbRbXxKR0xGcbIJGIIhwbMc+iG2yVDVegpRiPH/xxBcJYY3G1p7G+CwN+d9Ho7vNPvmcgheNN4JjRB7bVkoyAHoEbq0p9pNlfaYpIYktY/tfNQ872vGQzbpD+nl20w3bdSX0wdLJGp/m3AnehXciBzpYsNlbntF50emlLGylf1ARYTS5xwQv7fNd8Agf/3P1/AXQZ6Cl7pk3B8sVYpBPoMYbzYOmRk8AciUtH6IJFLRrz6c+3zsh+HlHIsfXX8XkuHtSa2STq0dnw35icJUxjbTKyNoWGFD6xdm8lXQYoKBBFncCUG3UmZSg0tGb9Uaxfd977YhevvIYTr6EE4pSNGqNir5F9DsSqD/FroGCj1p6xo0CJzHwFDFjvOzOhwTqtWpKHBj1/+5g==',
+  'NameIDFormat' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
+  'contacts' =>
+  array (
+    0 =>
+    array (
+      'emailAddress' => 'amakenadog@gmail.com',
+      'contactType' => 'technical',
+      'givenName' => 'Administrator',
+    ),
+  ),
+);
